a1c26e32c58c80540346c0d2288351b8c28d5d2a
yixiang zhao <hljtlzyx@live.com>
2020-07-31 16:23:16 +0800

    矩阵工具类新增方法 1、创建单位矩阵 2、矩阵求逆 3、求伴随矩阵

--- src/main/java/com/top/utils/MatrixUtil.java
+++ src/main/java/com/top/utils/MatrixUtil.java
@@ -3,6 +3,94 @@ package com.top.utils;
 import com.top.matrix.Matrix;
 
 public class MatrixUtil {
+    /**
+     * 创建一个单位矩阵
+     * @param matrixRowCount 单位矩阵的纬度
+     * @return
+     */
+    public static Matrix eye(int matrixRowCount){
+        double[][] result = new double[matrixRowCount][matrixRowCount];
+        for (int i = 0; i < matrixRowCount; i++) {
+            for (int j = 0; j < matrixRowCount; j++) {
+                if(i == j){
+                    result[i][j] = 1;
+                }else{
+                    result[i][j] = 0;
+                }
+            }
+        }
+        return new Matrix(result);
+    }
+
+    /**
+     * 求矩阵的逆
+     * 原理:AE=EA^-1
+     * @param a
+     * @return
+     * @throws Exception
+     */
+    public static Matrix inv(Matrix a) throws Exception {
+        if (a.getMatrixColCount() != a.getMatrixRowCount()) {
+            throw new Exception("矩阵不可逆");
+        }
+        // [a|E]
+        Matrix b = a.splice(1, eye(a.getMatrixRowCount()));
+        double[][] data = b.getMatrix();
+        int rowCount = b.getMatrixRowCount();
+        int colCount = b.getMatrixColCount();
+        //此处应用a的列数，为简化，直接用b的行数
+        for (int j = 0; j < rowCount; j++) {
+            //若遇到0则交换两行
+            int notZeroRow = -2;
+            if(data[j][j] == 0){
+                notZeroRow = -1;
+                for (int l = j; l < rowCount; l++) {
+                    if (data[l][j] != 0) {
+                        notZeroRow = l;
+                        break;
+                    }
+                }
+            }
+            if (notZeroRow == -1) {
+                throw new Exception("矩阵不可逆");
+            }else if(notZeroRow != -2){
+                //交换j与notZeroRow两行
+                double[] tmp = data[j];
+                data[j] = data[notZeroRow];
+                data[notZeroRow] = tmp;
+            }
+            //将第data[j][j]化为1
+            if (data[j][j] != 1) {
+                double multiple = data[j][j];
+                for (int colIdx = j; colIdx < colCount; colIdx++) {
+                    data[j][colIdx] /= multiple;
+                }
+            }
+            //行与行相减
+            for (int i = 0; i < rowCount; i++) {
+                if (i != j) {
+                    double multiple = data[i][j] / data[j][j];
+                    //遍历行中的列
+                    for (int k = j; k < colCount; k++) {
+                        data[i][k] = data[i][k] - multiple * data[j][k];
+                    }
+                }
+            }
+        }
+        Matrix result = new Matrix(data);
+        return result.subMatrix(0, rowCount, rowCount, rowCount);
+    }
+
+    /**
+     * 求矩阵的伴随矩阵
+     * 原理:A*=|A|A^-1
+     * @param a
+     * @return
+     * @throws Exception
+     */
+    public static Matrix adj(Matrix a) throws Exception {
+        return inv(a).multiple(det(a));
+    }
 
     /**
      * 矩阵转成上三角矩阵
@@ -40,10 +128,10 @@ public class MatrixUtil {
                     result[notZeroRow] = tmp;
                 }
 
-                double tmp = result[i][j]/result[j][j];
+                double multiple = result[i][j]/result[j][j];
                 //遍历行中的列
                 for (int k = j; k < matrixHeight; k++) {
-                    result[i][k] = result[i][k] - tmp * result[j][k];
+                    result[i][k] = result[i][k] - multiple * result[j][k];
                 }
             }
         }
