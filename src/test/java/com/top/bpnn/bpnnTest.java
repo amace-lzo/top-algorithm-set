afc5bb97c8a38c55185f93594e0a31aee9c41807
yixiang zhao <hljtlzyx@live.com>
2021-03-17 16:59:44 +0800

    Update bpnnTest.java

--- src/test/java/com/top/bpnn/bpnnTest.java
+++ src/test/java/com/top/bpnn/bpnnTest.java
@@ -14,24 +14,24 @@ import java.util.Date;
 public class bpnnTest {
     @Test
     public void test() throws Exception {
-        //创建训练集矩阵
+        // 创建训练集矩阵
         CsvInfo csvInfo = CsvUtil.getCsvInfo(false, "E:\\jarTest\\trainData.csv");
         Matrix trainSet = csvInfo.toMatrix();
-        //创建BPNN工厂对象
+        // 创建BPNN工厂对象
         BPNeuralNetworkFactory factory = new BPNeuralNetworkFactory();
-        //创建BP参数对象
+        // 创建BP参数对象
         BPParameter bpParameter = new BPParameter();
         bpParameter.setInputLayerNeuronCount(4);
         bpParameter.setHiddenLayerNeuronCount(5);
         bpParameter.setPrecision(0.01);
         bpParameter.setMaxTimes(100000);
 
-        //训练BP神经网络
+        // 训练BP神经网络
         System.out.println(new Date());
         BPModel bpModel = factory.trainBP(bpParameter, trainSet);
         System.out.println(new Date());
 
-        //将BPModel序列化到本地
+        // 将BPModel序列化到本地
         SerializationUtil.serialize(bpModel, "test");
 
         CsvInfo csvInfo2 = CsvUtil.getCsvInfo(false, "E:\\jarTest\\testData.csv");
@@ -39,7 +39,7 @@ public class bpnnTest {
 
         Matrix testData1 = testSet.subMatrix(0, testSet.getMatrixRowCount(), 0, testSet.getMatrixColCount() - 1);
         Matrix testLabel = testSet.subMatrix(0, testSet.getMatrixRowCount(), testSet.getMatrixColCount() - 1, 1);
-        //将BPModel反序列化
+        // 将BPModel反序列化
         BPModel bpModel1 = (BPModel) SerializationUtil.deSerialization("test");
         Matrix result = factory.computeBP(bpModel1, testData1);
 
@@ -62,9 +62,9 @@ public class bpnnTest {
     public void bpnnUsing() throws Exception{
         CsvInfo csvInfo = CsvUtil.getCsvInfo(false, "E:\\jarTest\\data.csv");
         Matrix data = csvInfo.toMatrix();
-        //将BPModel反序列化
+        // 将BPModel反序列化
         BPModel bpModel1 = (BPModel) SerializationUtil.deSerialization("test");
-        //创建工厂
+        // 创建工厂
         BPNeuralNetworkFactory factory = new BPNeuralNetworkFactory();
         Matrix result = factory.computeBP(bpModel1, data);
         CsvUtil.createCsvFile(null,result,"E:\\jarTest\\computeResult.csv");
